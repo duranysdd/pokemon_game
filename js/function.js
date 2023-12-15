b2131e1c91f1b0c3d61c103ef4740c29f4a4fede
duranysd <duranysd1119@gmail.com>
2023-12-15 13:23:43 -0600

    Save_05_addJS

---
+++ js/function.js
@@ -0,0 +1,8 @@
+let tarjetasDestapadas = 0;
+let tarjeta1 = null;
+let tarjeta2 = null;
+
+let mostrarMovimientos = document.getElementById
+
+let numeros = [1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8];
+numeros = numeros.sort(() => {return Math.random() -0.5});
