97980b0ae9798443366bc9e937604e65f367868c
duranysd <duranysd1119@gmail.com>
2023-12-15 13:29:28 -0600

    Save_06

--- js/function.js
+++ js/function.js
@@ -1,8 +1,24 @@
 let tarjetasDestapadas = 0;
 let tarjeta1 = null;
 let tarjeta2 = null;
+let primerResultado = null;
+let segundoResultado = null;
+let movimientos = 0;
+let aciertos = 0;
+let temporizador = false;
+let timer = 50;
+let timerinicial = 50;
 
-let mostrarMovimientos = document.getElementById
+let mostrarMovimientos = document.getElementById("movimientos");
+
+let mostrarAciertos =  document.getElementById("aciertos");
+
+let mostrarTiempo = document.getElementById("t_restante");
 
 let numeros = [1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8];
 numeros = numeros.sort(() => {return Math.random() -0.5});
+
+
+function contarTiempo(){
+
+}
